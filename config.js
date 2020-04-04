0952535d3669e1cb2026660671f3a9e111ba8da6
Waner Deng <dengwaner@gmail.com>
2020-04-03 17:22:00 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook' : {
+        clientId: '676712912895512',
+        clientSecret: 'da72ce1b82f24185b1835764f6b6c31a'
+    }
 }
